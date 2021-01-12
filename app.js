2271a31aed211fe510fe447aa18eda3b77f71d20
Yash Vadi <yvadi9274@yahoo.com>
2021-01-12 19:08:39 +0530

    added 'No.of school' and 'No.of proffesor' in admin-dashboard

--- app.js
+++ app.js
@@ -210,8 +210,9 @@ app.get("/:schoolname/admin/dashboard", function(req, res) {
 
     if (req.isAuthenticated() && req.user.role == "admin" && req.user.schoolshort == schoolname) {
         School.findOne({ shortname: schoolname }, function(err, find) {
+            console.log(find.studentid.length);
             Course.find({ schoolid: find._id }, function(err, found) {
-                res.render("admin_dash", { school: schoolname, courses: found, message: "" });
+                res.render("admin_dash", { school: schoolname, courses: found, no_student:find.studentid.length , no_professor:find.professorid.length ,message: "" });
             })
         })
     } else {
